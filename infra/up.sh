2e0b0935ed16ac576a4143dfc82c2c1a8c2357dd
Lucas Vicente <lucasvicentedesouza021@gmail.com>
2026-07-10 12:08:06 -0300

    feat: initialize infrastructure with Kubernetes manifests, NGINX configuration, and orchestrate services via Docker Compose and Kind setup scripts

--- infra/up.sh
+++ infra/up.sh
@@ -9,6 +9,29 @@ kind delete cluster --name greg-company-cluster || true
 echo "🏗️ Criando novo cluster Kind..."
 kind create cluster --config kind-config.yaml
 
+echo "🔨 Iniciando Build das imagens locais..."
+echo "-> Construindo Portal..."
+(cd ../portal && docker build -t infra-frontend-portal:latest .)
+
+echo "-> Construindo Admin..."
+(cd ../admin && docker build -t infra-frontend-admin:latest .)
+
+echo "-> Construindo Backend..."
+(cd ../backend && docker build -t infra-backend:latest .)
+
+echo "-> Construindo Bot..."
+(cd ../ecommerce-bot && docker build -t ecommerce-bot:latest .)
+
+echo "-> Construindo Go Worker..."
+(cd ../go-worker && docker build -t infra-go-worker:latest .)
+
+echo "📥 Injetando imagens no cluster Kind..."
+kind load docker-image infra-frontend-portal:latest --name greg-company-cluster
+kind load docker-image infra-frontend-admin:latest --name greg-company-cluster
+kind load docker-image infra-backend:latest --name greg-company-cluster
+kind load docker-image ecommerce-bot:latest --name greg-company-cluster
+kind load docker-image infra-go-worker:latest --name greg-company-cluster
+
 echo "🌐 Instalando NGINX Ingress Controller para Kind..."
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
